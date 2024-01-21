c067a82bc8b7300323376ec8615da34fd270f383
Adam Cichy <adam_cichy@outlook.com>
2024-01-21 23:53:55 +0000

    improve schema for alerts - WIP

--- migrations/ddl/schema.sql
+++ migrations/ddl/schema.sql
@@ -1,14 +1,14 @@
 CREATE TABLE MonitoredServices (
-    ServiceId STRING(36) DEFAULT (GENERATE_UUID()),
-    Url STRING(1024),
+    ServiceId STRING(36) NOT NULL DEFAULT (GENERATE_UUID()),
+    Url STRING(1024) NOT NULL,
     Frequency INT64 NOT NULL,
     AlertingWindow INT64 NOT NULL,
     AllowedResponseTime INT64 NOT NULL
 ) PRIMARY KEY (ServiceId);
 
 CREATE TABLE MonitoredServicesLease (
-    ServiceId STRING(36),
-    MonitorId STRING(36),
+    ServiceId STRING(36) NOT NULL,
+    MonitorId STRING(36) NOT NULL,
     LeasedAt TIMESTAMP NOT NULL,
     LeaseDurationMs INT64 NOT NULL,
     LeasedTo TIMESTAMP NOT NULL AS (TIMESTAMP_MILLIS(UNIX_MILLIS(LeasedAt) + LeaseDurationMs)) STORED
@@ -16,18 +16,17 @@ CREATE TABLE MonitoredServicesLease (
 
 CREATE NULL_FILTERED INDEX LeaseByMonitorId ON MonitoredServicesLease(MonitorId, ServiceId);
 
-CREATE TABLE Detections (
-    ServiceId STRING(36),
-    LastDetectionTime TIMESTAMP
-) PRIMARY KEY (ServiceId);
-
 CREATE TABLE Alerts (
-    AlertId STRING(36) DEFAULT (GENERATE_UUID()),
-    ServiceId STRING(36),
-    MonitorId STRING(36),
+    ShardId INT64 NOT NULL AS (MOD(FARM_FINGERPRINT(ServiceId), 32) + 31) STORED,
+    AlertId STRING(36) NOT NULL DEFAULT (GENERATE_UUID()),
+    ServiceId STRING(36) NOT NULL,
+    MonitorId STRING(36) NOT NULL,
     DetectionTimestamp TIMESTAMP NOT NULL,
+    StatusExpirationTimestamp TIMESTAMP,
     AlertStatus INT64 NOT NULL
 
-) PRIMARY KEY (ServiceId, DetectionTimestamp DESC);
+) PRIMARY KEY (ShardId, ServiceId, DetectionTimestamp DESC);
 
-CREATE NULL_FILTERED INDEX AlertsById ON Alerts(AlertId)
+CREATE NULL_FILTERED INDEX AlertsById ON Alerts(AlertId);
+
+CREATE NULL_FILTERED INDEX AlertsByStatus ON Alerts(AlertStatus, AlertId)
--- migrations/dml/example.sql
+++ migrations/dml/example.sql
@@ -1,2 +1,3 @@
 INSERT INTO MonitoredServices (ServiceId, Url, Frequency, AlertingWindow, AllowedResponseTime)
 VALUES ('6839b274-f5ab-42f5-a3f8-ea10bbf2b599', "http://localhost:8000/", 10000, 5000, 120000)
+THEN RETURN ServiceId
