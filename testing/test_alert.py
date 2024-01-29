7da011ed85fc6d3dcdc58ca224de13b614e71896
Adam Cichy <adam_cichy@outlook.com>
2024-01-29 11:07:29 +0000

    test_monitor_alerter_admin_workflow

--- testing/test_alert.py
+++ testing/test_alert.py
@@ -4,7 +4,7 @@ import os
 import time
 import logging
 from .settings import MAIN_URL
-from .types import Alert, ServiceId
+from .types import Alert, ServiceId, AlertStatus, AlerterId
 
 TIMEOUT = 2.0
 
@@ -26,27 +26,17 @@ def get_service_alerts(serviceId: str) -> list[Alert]:
     return [Alert.model_validate(x) for x in r.json()]
 
 
+def ack_alert(alertId: AlerterId):
+    r = requests.post(f"{MAIN_URL}alerts/{alertId}/ack/", timeout=TIMEOUT)
+    assert r.status_code == 200
+
+
 def sleep(s: float):
     logging.info(f"Sleeping for {s // 60:.0f} min {s % 60:.0f} s")
     time.sleep(s)
 
 
-def test_monitor_inserts_alert_for_unresponding_service(caplog):
-    caplog.set_level(logging.INFO)
-    clear_environment()
-    # Mock alert data
-    data = {
-        "url": "https://martinez-alert.com/",
-        "frequency": 10_000,
-        "alertingWindow": 20_000,
-        "allowedResponseTime": 120_000,
-        "contact_methods": [
-            {"email": "default1@example.com"},
-            {"email": "default2@example.com"},
-        ],
-    }
-
-    serviceId = insert_service(data)
+def monitor_inserts_alert_for_unresponding_service(serviceId: ServiceId):
     assert (len(get_service_alerts(serviceId))) == 0
     alert_found = None
 
@@ -61,6 +51,63 @@ def test_monitor_inserts_alert_for_unresponding_service(caplog):
 
     assert alert_found is not None
     logging.info(f"Found alert for service after {alert_found} attempts")
+
+
+def alerter_sends_alert_to_first_admin(serviceId: ServiceId):
+    first_admin_notified = None
+    for i in range(8):
+        alerts = get_service_alerts(serviceId)
+        assert len(alerts) == 1
+        alert = alerts[0]
+        if alert.status == AlertStatus.NOTIFY1:
+            first_admin_notified = i + 1
+            break
+        logging.info(f"Waiting for alerter. Sleeping for the {i+1}th time")
+        sleep(10.0)
+
+    assert first_admin_notified is not None
+    logging.info(f"Found alert notification after {first_admin_notified} attempts")
+
+
+def ensure_alert_status(alertId: AlerterId, serviceId: ServiceId, status: AlertStatus):
+    alerts = get_service_alerts(serviceId)
+    alert = [a for a in alerts if a.alertId == alertId][0]
+    assert alert.status == status
+
+
+def alert_is_ack_after_ack_by_admin(serviceId: ServiceId):
+    alerts = get_service_alerts(serviceId)
+    assert len(alerts) >= 1
+    alert = alerts[0]
+    ack_alert(alert.alertId)
+    ensure_alert_status(alert.alertId, serviceId, AlertStatus.ACK)
+    logging.info(
+        f"Waiting some time to ensure that monitors/alerters do not modify ACKed alert"
+    )
+    sleep(120.0)
+    ensure_alert_status(alert.alertId, serviceId, AlertStatus.ACK)
+
+
+def test_monitor_alerter_admin_workflow(caplog):
+    caplog.set_level(logging.INFO)
+
+    clear_environment()
+    # Mock alert data
+    data = {
+        "url": "https://martinez-alert.com/",
+        "frequency": 10_000,
+        "alertingWindow": 20_000,
+        "allowedResponseTime": 120_000,
+        "contact_methods": [
+            {"email": "default1@example.com"},
+            {"email": "default2@example.com"},
+        ],
+    }
+
+    serviceId = insert_service(data)
+    monitor_inserts_alert_for_unresponding_service(serviceId)
+    alerter_sends_alert_to_first_admin(serviceId)
+    alert_is_ack_after_ack_by_admin(serviceId)
 
 
 def test_monitor_does_not_insert_alert_for_properly_working_service(caplog):
